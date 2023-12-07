8b169b29f2be4e7ba65cb084689e0d79e6109b02
koralle <mkhd7930@gmail.com>
2023-12-08 00:29:19 +0900

    chore: biome format --write

--- app/root.tsx
+++ app/root.tsx
@@ -1,17 +1,10 @@
-import type { LinksFunction } from "@remix-run/cloudflare";
-import { cssBundleHref } from "@remix-run/css-bundle";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import type { LinksFunction } from '@remix-run/cloudflare'
+import { cssBundleHref } from '@remix-run/css-bundle'
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+  ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
+]
 
 export default function App() {
   return (
@@ -29,5 +22,5 @@ export default function App() {
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
