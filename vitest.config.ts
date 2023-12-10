e9be21c6bf71092bc0364c90845fcbd7a8953107
koralle <mkhd7930@gmail.com>
2023-12-10 19:20:01 +0900

    tiny fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,9 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     setupFiles: ['./vitest.setup.ts'],
+    alias: {
+      '~/': new URL('./app', import.meta.url).pathname,
+      '~tests/': new URL('./tests', import.meta.url).pathname,
+    },
   },
 })
