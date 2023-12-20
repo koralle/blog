5a1d7908e928f2937fd277ba45b92fc1173678c9
koralle <mkhd7930@gmail.com>
2023-12-21 00:45:39 +0900

    add: app/utils/adapters.server.ts

---
+++ app/utils/adapters.server.ts
@@ -0,0 +1,35 @@
+import type { AppLoadContext, ServerBuild } from '@remix-run/server-runtime'
+import type { Context, Env, Input, MiddlewareHandler } from 'hono'
+
+import { createRequestHandler } from '@remix-run/server-runtime'
+
+export interface RemixMiddlewareOptions<
+  E extends Env = Record<string, never>,
+  P extends string = '',
+  I extends Input = Record<string, never>,
+> {
+  build: ServerBuild
+  mode?: 'development' | 'production'
+  getLoadContext?(event: Context<E, P, I>): Promise<AppLoadContext> | AppLoadContext
+}
+
+export function remix<
+  E extends Env = Record<string, never>,
+  P extends string = '',
+  I extends Input = Record<string, never>,
+>({
+  mode,
+  build,
+  getLoadContext = (context) => context.env as unknown as AppLoadContext,
+}: RemixMiddlewareOptions<E, P, I>): MiddlewareHandler<E, P, I> {
+  return async function middleware(context) {
+    const requestHandler = createRequestHandler(build, mode)
+    const loadContext = getLoadContext(context)
+    return await requestHandler(
+      context.req.raw,
+      loadContext instanceof Promise ? await loadContext : loadContext,
+    )
+  }
+}
+
+export { createRequestHandler } from '@remix-run/server-runtime'
