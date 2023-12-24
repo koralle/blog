96c223e2370718ac7926931257e1791c5057aa3d
koralle <mkhd7930@gmail.com>
2023-12-25 01:47:58 +0900

    add: test codes for CMSClient

--- tests/mocks/handlers.ts
+++ tests/mocks/handlers.ts
@@ -1,5 +1,6 @@
 import { HttpHandler } from 'msw'
+import { fetchContentList, fetchSingleContent } from './api/v1/blogs'
 
 type HttpHandlers = readonly HttpHandler[]
 
-export const handlers: HttpHandlers = []
+export const handlers: HttpHandlers = [fetchSingleContent, fetchContentList]
