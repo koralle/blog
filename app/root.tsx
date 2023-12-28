6a0e5215672f6399b9502b22c60a2e3e932b5a07
koralle <mkhd7930@gmail.com>
2023-12-28 22:33:19 +0900

    wip

--- app/root.tsx
+++ app/root.tsx
@@ -2,9 +2,11 @@ import type { LinksFunction } from '@remix-run/cloudflare'
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
 import resetCss from 'the-new-css-reset/css/reset.css'
 import radixThemeCss from '@radix-ui/themes/styles.css'
-import { Theme } from '@radix-ui/themes'
 import { cssBundleHref } from '@remix-run/css-bundle'
 import { bodyRoot, htmlRoot } from '~/global.css'
+import { Box, Container, Grid, Theme } from '@radix-ui/themes'
+import { Header } from '~/components/header'
+import { Footer } from '~/components/footer'
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
@@ -22,10 +24,21 @@ export default function App() {
         <Links />
       </head>
       <body className={bodyRoot}>
-        <Theme appearance="light">
-          <header></header>
-          <Outlet />
-          <footer></footer>
+        <Theme appearance="dark" style={{ height: '100%' }}>
+          <Grid
+            rows="3"
+            columns="1"
+            display="grid"
+            style={{ height: '100%', gridTemplateRows: 'auto 1fr auto' }}
+          >
+            <header>
+              <Header />
+            </header>
+            <Container>
+              <Outlet />
+            </Container>
+            <Footer />
+          </Grid>
           <ScrollRestoration />
           <Scripts />
           <LiveReload />
