54352df4896f15b9de36c159d424388dc355da3e
koralle <mkhd7930@gmail.com>
2023-12-29 01:19:07 +0900

    wip: app/routes/articles.$articleId.tsx

--- app/routes/articles.$articleId.tsx
+++ app/routes/articles.$articleId.tsx
@@ -5,6 +5,7 @@ import type { EventContext } from '@cloudflare/workers-types'
 import { json, MetaFunction } from '@remix-run/cloudflare'
 import { cmsBlogContentSchema } from '~/cms/schema'
 import { useLoaderData } from '@remix-run/react'
+import { Box } from '@radix-ui/themes'
 
 export const meta: MetaFunction = () => {
   return []
@@ -40,12 +41,12 @@ const Index = () => {
   const data = useLoaderData<typeof loader>()
 
   return (
-    <div>
+    <Box px="4">
       <h1>
         <span>{data.title}</span>
       </h1>
       <div>{data.content ? parse(data.content) : ''}</div>
-    </div>
+    </Box>
   )
 }
 
