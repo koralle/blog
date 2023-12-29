9d02d57ee9a6a99d06189e993e68ea5a33423543
koralle <mkhd7930@gmail.com>
2023-12-30 02:50:11 +0900

    wip: global.css.ts

--- app/global.css.ts
+++ app/global.css.ts
@@ -1,9 +1,17 @@
 import { style } from '@vanilla-extract/css'
 
-export const htmlRoot = style({
+export const htmlRootStyles = style({
   height: '100%',
 })
 
-export const bodyRoot = style({
+export const bodyRootStyles = style({
   height: '100%',
 })
+
+export const themeRootStyles = style({
+  height: '100%',
+})
+
+export const gridRootStyles = style({})
+
+export const headerRootStyles = style({})
