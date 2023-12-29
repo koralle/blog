f4240ef6ebfd36e9750be1f465dc8c4ad26376cb
koralle <mkhd7930@gmail.com>
2023-12-30 02:50:05 +0900

    wip: consts

--- app/consts.ts
+++ app/consts.ts
@@ -1,3 +1,5 @@
-const SITE_TITlE = 'Hello, world!' as const
+const SITE_TITlE = "koralle's tech blog" as const
 
-export { SITE_TITlE }
+const SITE_DESCRIPTION = '布団の中で働きたいあざらしのtech blog' as const
+
+export { SITE_DESCRIPTION, SITE_TITlE }
