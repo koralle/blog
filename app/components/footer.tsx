bf0eb83a4f8245dedc6ae865069976964e46a44c
koralle <mkhd7930@gmail.com>
2023-12-30 02:49:52 +0900

    wip: footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,20 +1,63 @@
-import { Box, Container, Flex, Grid, Heading, Section, Separator, Text } from '@radix-ui/themes'
-import { Link } from '@remix-run/react'
-import { SITE_TITlE } from '~/consts'
+import { Box, Container, Grid, Link, Separator, Text } from '@radix-ui/themes'
+import { Link as RemixLink } from '@remix-run/react'
+import { SITE_DESCRIPTION, SITE_TITlE } from '~/consts'
 
 const Footer = () => {
   return (
-    <Box width="100%" px="4" pt="4" style={{ alignSelf: 'end' }}>
+    <Box width="100%" px="4" style={{ alignSelf: 'end' }}>
       <Container>
-        <Link to="/" target="_self">
-          {SITE_TITlE}
-        </Link>
+        <Box py="4">
+          <Box>
+            <Link asChild weight="bold" color="gray">
+              <RemixLink to="/" target="_self">
+                <Text size="5">{SITE_TITlE}</Text>
+              </RemixLink>
+            </Link>
+          </Box>
+          <Text size="3">{SITE_DESCRIPTION}</Text>
+        </Box>
         <Grid columns="2">
           <Box>
-            <Text as="span">About</Text>
+            <Box>
+              <Text as="span" weight="bold">
+                About
+              </Text>
+            </Box>
+            <Box py="2">
+              <Link asChild color="gray">
+                <RemixLink to="/" target="_blank" referrerPolicy="no-referrer">
+                  <Text as="span">このブログについて</Text>
+                </RemixLink>
+              </Link>
+            </Box>
+            <Box py="2">
+              <Link asChild color="gray">
+                <RemixLink to="/" target="_blank" referrerPolicy="no-referrer">
+                  <Text as="span">koralleについて</Text>
+                </RemixLink>
+              </Link>
+            </Box>
           </Box>
           <Box>
-            <Text as="span">Links</Text>
+            <Box>
+              <Text as="span" weight="bold">
+                Links
+              </Text>
+            </Box>
+            <Box py="2">
+              <Link asChild color="gray">
+                <RemixLink to="/" target="_blank" referrerPolicy="no-referrer">
+                  <Text as="span">GitHub</Text>
+                </RemixLink>
+              </Link>
+            </Box>
+            <Box py="2">
+              <Link asChild color="gray">
+                <RemixLink to="/" target="_blank" referrerPolicy="no-referrer">
+                  <Text as="span">Twitter</Text>
+                </RemixLink>
+              </Link>
+            </Box>
           </Box>
         </Grid>
       </Container>
