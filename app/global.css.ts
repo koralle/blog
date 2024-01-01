bffd3e328f7cc7d7d3971969a28e5a92ec3d9b52
koralle <mkhd7930@gmail.com>
2024-01-01 21:50:36 +0900

    chore(css): append font-styles to 'html'

--- app/global.css.ts
+++ app/global.css.ts
@@ -2,6 +2,10 @@ import { style } from '@vanilla-extract/css'
 
 export const htmlRoot = style({
   height: '100%',
+  fontFamily: "'Noto Sans JP', sans-serif",
+  fontSize: '10px',
+  fontStyle: 'normal',
+  fontVariant: 'normal',
 })
 
 export const bodyRoot = style({
