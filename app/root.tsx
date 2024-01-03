7395c0fe13ad005f48c7022c4fae42f109b3bd2c
koralle <mkhd7930@gmail.com>
2024-01-03 23:03:59 +0900

    chore: app/root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -2,10 +2,10 @@ import type { LinkDescriptor, LinksFunction } from '@remix-run/cloudflare'
 import { Links, LiveReload, Meta, Outlet, Scripts } from '@remix-run/react'
 import resetCss from 'the-new-css-reset/css/reset.css'
 import radixThemeCss from '@radix-ui/themes/styles.css'
-import { Theme } from '@radix-ui/themes'
 import { cssBundleHref } from '@remix-run/css-bundle'
 import { bodyRoot, htmlRoot } from '~/global.css'
 import { Layout } from '~/layout'
+import { AppearanceProvider } from '~/contexts/AppearanceProvider'
 
 const WebFontLinks: LinkDescriptor[] = [
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
@@ -37,9 +37,11 @@ export default function App() {
         <Links />
       </head>
       <body className={bodyRoot}>
-        <Layout>
-          <Outlet />
-        </Layout>
+        <AppearanceProvider>
+          <Layout>
+            <Outlet />
+          </Layout>
+        </AppearanceProvider>
         <LiveReload />
         <Scripts />
       </body>
