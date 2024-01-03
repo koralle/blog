f380baa2bf3dd4a1ccca182c3dc5ea73df5d7072
koralle <mkhd7930@gmail.com>
2024-01-03 23:03:39 +0900

    chore: app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,31 @@
 import { Box, Grid, Theme } from '@radix-ui/themes'
 import { Outlet } from '@remix-run/react'
-import { ReactNode } from 'react'
+import { ReactNode, useContext } from 'react'
+import { Header } from '~/components/header'
+import { AppearanceContext } from '~/contexts/AppearanceContext'
 
-const Layout = ({ children }: { children: ReactNode }) => (
-  <Theme asChild appearance="light">
-    <Grid flow="column" height="100%" rows="min-content 1fr min-content">
-      <Box asChild>
-        <header>Hello, world!</header>
-      </Box>
-      <Box asChild>
-        <main>
-          <Outlet />
-        </main>
-      </Box>
-      <Box asChild>
-        <footer>This is a footer.</footer>
-      </Box>
-    </Grid>
-  </Theme>
-)
+const Layout = ({ children }: { children?: ReactNode }) => {
+  const appearance = useContext(AppearanceContext)
+
+  return (
+    <Theme asChild appearance={appearance}>
+      <Grid flow="column" height="100%" rows="min-content 1fr min-content">
+        <Box asChild>
+          <header>
+            <Header />
+          </header>
+        </Box>
+        <Box asChild>
+          <main>
+            <Outlet />
+          </main>
+        </Box>
+        <Box asChild>
+          <footer>This is a footer.</footer>
+        </Box>
+      </Grid>
+    </Theme>
+  )
+}
 
 export { Layout }
