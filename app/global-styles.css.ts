34de27c2c6453f66d91756787485ddefede31e89
koralle <mkhd7930@gmail.com>
2024-01-10 23:46:14 +0900

    add: global styles to `<img />`

--- app/global-styles.css.ts
+++ app/global-styles.css.ts
@@ -10,3 +10,13 @@ globalStyle('html', {
 globalStyle('body', {
   height: '100%',
 })
+
+globalStyle('img', {
+  maxWidth: '100%',
+  height: 'auto',
+  verticalAlign: 'middle',
+  fontStyle: 'italic',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  shapeMargin: '0.75rem',
+})
