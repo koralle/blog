365a909167bd5ac3f4cfae908bed4a3227d1a0ff
koralle <mkhd7930@gmail.com>
2024-01-10 23:00:35 +0900

    add: `<meta name="color-scheme" content="dark light" />`

--- app/root.tsx
+++ app/root.tsx
@@ -30,6 +30,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="color-scheme" content="dark light" />
         <Meta />
         <Links />
       </head>
