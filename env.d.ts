f5e7061dc9236998da5d318a0a98b35310ab72d7
koralle <mkhd7930@gmail.com>
2024-03-24 23:06:04 +0900

    chore: remove remix.env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,7 @@
+/// <reference types="@remix-run/dev" />
+/// <reference types="@remix-run/cloudflare" />
+/// <reference types="@cloudflare/workers-types" />
+
 declare type Env = {
   MICROCMS_API_KEY: string
   MICROCMS_SERVICE_NAME: string
